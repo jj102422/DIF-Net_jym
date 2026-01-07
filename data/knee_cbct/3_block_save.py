7f42e2cf166a85f953c7d3a4a0c1e56fd9ff528b
bnbqq8 <bnbqq8@i.smu.edu.cn>
2026-01-07 10:33:35 +0000

    fix block gen for varied z indices length
    Signed-off-by: bnbqq8 <bnbqq8@i.smu.edu.cn>

--- data/knee_cbct/3_block_save.py
+++ data/knee_cbct/3_block_save.py
@@ -1,8 +1,10 @@
 import os
-import numpy as np
 from glob import glob
-from tqdm import tqdm
+
+import numpy as np
 import SimpleITK as sitk
+from tqdm import tqdm
+
 
 def read_nifti(path):
     itk_img = sitk.ReadImage(path)
@@ -15,9 +17,9 @@ def save_nifti(image, path):
     sitk.WriteImage(out, path)
 
 
-def generate_blocks():
+def generate_blocks(shape):
     block_list = []
-    base = np.mgrid[:64, :64, :64] * 4 # 3, 64 ^ 3
+    base = np.mgrid[: shape[0] // 4, : shape[1] // 4, : shape[2] // 4] * 4  # 3, 64 ^ 3
     base = base.reshape(3, -1)
     for x in range(4):
         for y in range(4):
@@ -28,23 +30,22 @@ def generate_blocks():
     return block_list
 
 
-if __name__ == '__main__':
-    os.makedirs('./blocks/', exist_ok=True)
+if __name__ == "__main__":
 
-    block_list = generate_blocks()
-    blocks = np.stack(block_list, axis=0) # K, 3, N^3
-    blocks = blocks.transpose(0, 2, 1).astype(float) / 255 # K, N^3, 3
-    np.save('./blocks/blocks.npy', blocks)
-    
-    files = glob(f'processed/*.nii.gz')
+    files = glob(f"processed/*.nii.gz")
     for file in tqdm(files, ncols=50):
-        name = file.split('/')[-1].split('.')[0]
-        data_path = f'./processed/{name}.nii.gz'
+        name = file.split("/")[-1].split(".")[0]
+        data_path = f"./processed/{name}.nii.gz"
         image = read_nifti(data_path)
 
-        save_dir = f'./blocks/{name}/'
+        save_dir = f"./blocks/{name}/"
         os.makedirs(save_dir, exist_ok=True)
+
+        block_list = generate_blocks(image.shape)
+        blocks = np.stack(block_list, axis=0)  # K, 3, N^3
+        blocks = blocks.transpose(0, 2, 1).astype(float) / 255  # K, N^3, 3
+        np.savez(os.path.join(save_dir, f"blocks.npz"), blocks=blocks)
         for k, block in enumerate(block_list):
             block = block.reshape(3, -1).transpose(1, 0)
             image_block = image[block[:, 0], block[:, 1], block[:, 2]]
-            np.save(os.path.join(save_dir, f'block_{k}.npy'), image_block)
+            np.savez(os.path.join(save_dir, f"block_{k}.npz"), image_block)
